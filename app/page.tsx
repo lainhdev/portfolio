90cb9c47c95e42699cf1c402dc9721c3f5b06bd9
huulai2510 <huulai2510@gmail.com>
2023-07-10 13:24:12 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,177 @@
-import Image from 'next/image'
+"use client";
+
+import Animation from "@/components/animation";
+import { Github, MoveRight } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="w-screen max-w-screen-lg mx-auto px-5 pt-20">
+      <div className="flex flex-rows items-center justify-center">
+        <a
+          href="https://github.com/lainhdev"
+          className="bg-white rounded-full p-6 mr-10"
+        >
+          <Image src="/github.svg" width={60} height={60} alt="github" />
+        </a>
+        <a
+          href="mailto:lainh.dev@gmail.com"
+          className="bg-white rounded-full p-5 py-7"
+        >
+          <Image
+            src="/logos_google-gmail.svg"
+            width={60}
+            height={60}
+            alt="gmail"
+          />
+        </a>
+      </div>
+      <p className="text-[3.5rem] md:text-[5rem] font-bold stroke-slate-500 text-transparent stroke-1 stroke-im">
+        Hello! I&#39;m Lai
+      </p>
+      <p className="text-[3rem] md:text-[4rem] font-bold text-white">
+        I&#39;m developing Creative & Interactive webapps
+      </p>
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <p className="w-full md:w-1/2 text-white/60 font-Glimer-Light xl:text-[1.3rem] 2xl:text-[1.5rem] lg:text-base md:text-sm text-xs xl:leading-8 2xl:leading-9 lg:leading-6 leading-5 mb-6 md:mb-0">
+          As a developer, my expertise lies in creating seamless and intuitive
+          user experiences. I specialize in using cutting-edge technologies to
+          produce high-quality web applications. Specifically, I have vast
+          knowledge of ReactJS and NodeJS, which allow me to build complex and
+          dynamic applications with ease.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="w-full md:w-1/2">
+          <Animation />
         </div>
       </div>
+      <div>
+        <p className="text-[3.5rem] md:text-[5rem] font-bold stroke-slate-500 text-transparent stroke-1 stroke-im">
+          Featured Project
+        </p>
+        <div className="flex flex-col md:flex-row items-center justify-around mt-20">
+          <Image src="/evira.png" alt="evira" width={500} height={500} />
+          <div className="md:w-1/2 flex items-center justify-center">
+            <div className=" text-white">
+              <p className="text-3xl text-white font-bold mb-5 mt-5">
+                Evira Store
+              </p>
+              <p className="text-xl mb-3">
+                an online fashion store and <br />
+                an admin dashboard to manage
+              </p>
+              <div className="flex flex-row items-center mb-2">
+                <span>Store:</span>{" "}
+                <a href="https://github.com/lainhdev/evira-store">
+                  <Github width={40} />
+                </a>
+                <a href="https://evira-store-6t8j.vercel.app/">
+                  <MoveRight width={40} />
+                </a>
+              </div>
+              <div className="flex flex-row items-center">
+                <span>Admin:</span>{" "}
+                <a href="https://github.com/lainhdev/ecommerce-admin">
+                  <Github width={40} />
+                </a>
+                <a href="https://ecommerce-admin-lainh-1x2lpjs3q-huulai.vercel.app/">
+                  <MoveRight width={40} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row-reverse items-center justify-between mt-20">
+          <Image src="/whats-up.png" alt="whats-up" width={500} height={500} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          <div className="md:w-1/2 flex items-center justify-center">
+            <div className=" text-white">
+              <p className="text-3xl text-white font-bold mb-5 mt-5">
+                Whats Up
+              </p>
+              <p className="text-xl mb-3">an online chatting app</p>
+              <div className="flex flex-row items-center mb-2">
+                <span>App:</span>{" "}
+                <a href="https://github.com/lainhdev/whatsup-client">
+                  <Github width={40} />
+                </a>
+                <a href="https://whatsup-buqs.onrender.com/">
+                  <MoveRight width={40} />
+                </a>
+              </div>
+              <div className="flex flex-row items-center">
+                <span>Service:</span>{" "}
+                <a href="https://github.com/lainhdev/whatsup-api">
+                  <Github width={40} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-between mt-20">
+          <Image src="/zomo.png" alt="zomo" width={500} height={500} />
+
+          <div className="md:w-1/2 flex items-center justify-center">
+            <div className=" text-white">
+              <p className="text-3xl text-white font-bold mb-5 mt-5">Zomo</p>
+              <p className="text-xl mb-3">a video conference & meeting app</p>
+              <div className="flex flex-row items-center mb-2">
+                <span>App:</span>{" "}
+                <a href="https://github.com/lainhdev/zomo-client">
+                  <Github width={40} />
+                </a>
+                <a href="https://zomo.onrender.com/">
+                  <MoveRight width={40} />
+                </a>
+              </div>
+              <div className="flex flex-row items-center">
+                <span>Service:</span>{" "}
+                <a href="https://github.com/lainhdev/zomo-api">
+                  <Github width={40} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mt-20">
+        <p className="text-[3.5rem] md:text-[5rem] font-bold stroke-slate-500 text-transparent stroke-1 stroke-im">
+          My Skills
+        </p>
+        <div className="text-white grid-cols-3 grid text-center leading-loose">
+          <div>
+            <p className="text-xl font-bold">FrontEnd</p>
+            <p>Html</p>
+            <p>Css</p>
+            <p>Javascript</p>
+            <p>Redux</p>
+            <p>Nextjs</p>
+            <p>Tailwindcss</p>
+          </div>
+          <div>
+            <p className="text-xl font-bold">BackEnd</p>
+            <p>Nodejs</p>
+            <p>Nestjs</p>
+            <p>Typescript</p>
+            <p>Prisma</p>
+            <p>Graphql</p>
+            <p>Postgres</p>
+          </div>
+          <div>
+            <p className="text-xl font-bold">Tools</p>
+            <p>Git</p>
+            <p>VSCode</p>
+            <p>Figma</p>
+            <p>Render</p>
+            <p>Vercel</p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-20">
+        <p className="text-[3.5rem] md:text-[5rem] font-bold text-white">
+          Interested ?<br /> Let&#39;s Get In Touch !
+        </p>
+        <a href='/CV-NguyenHuuLai.pdf' className="inline-block text-white border border-white rounded-full px-3 py-2 mb-80 mt-10">Download Resume</a>
       </div>
     </main>
-  )
+  );
 }
---
+++ components/animation.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "@/json/walk.json";
+
+const Animation = () => <Lottie animationData={groovyWalkAnimation} loop={true} />;
+
+export default Animation;
